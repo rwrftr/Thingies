192ba3d2607afb00caa60b4125535b18ade95f11
rwrftr <104785633+rwrftr@users.noreply.github.com>
2022-11-10 13:43:47 -0800

    Update Thingy.java

--- Thingy.java
+++ Thingy.java
@@ -1,9 +1,25 @@
 class Thingy
 {
   private int myX, myY;
+  public int X2, Y2;
   public Thingy()
   {
     myX = myY = 0;
   }
-  /* your java code for "getters" and "setters" here */
+  
+  public void setX (int z){
+   
+    myX = z;
+    
+  }
+  public void setY (int a){
+    myY = a; 
+  }
+  
+  public int getX (){
+    return(myX); 
+  }
+  public int getY (){
+    return(myY); 
+  }
 }
